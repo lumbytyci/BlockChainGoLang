128d8948af7764ff41f559c7a09eb825eae8b6a4
lumbytyci <lumbytyci@hotmail.com>
2018-10-16 22:58:38 +0200

    gitignore

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ func main() {
 	BlockChain = initializeBlockChain()
 	newBlock1 := Block{"1", len(BlockChain), time.Now().String(), "noHash", "prevHash", "notx", []string{"notx"}, 0, 4}
 	newBlock2 := Block{"1", len(BlockChain), time.Now().String(), "noHash", "prevHash", "notx", []string{"notx"}, 0, 4}
-
+	newBlock3 := Block{"1", len(BlockChain), time.Now().String(), "noHash", "prevHash", "notx", []string{"notx"}, 0, 4}
 	appendBlock(newBlock1)
 	appendBlock(newBlock2)
+	appendBlock(newBlock3)
 
 	for i := 0; i < len(BlockChain); i++ {
 		fmt.Println("HASH: ", BlockChain[i].hash)
